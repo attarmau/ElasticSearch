38111e7043a4458763b5d023841db268ee39088f
attarmau <87364730+attarmau@users.noreply.github.com>
2023-04-18 06:00:58 +0800

    Update data indexing.java

--- data indexing.java
+++ data indexing.java
@@ -2,3 +2,7 @@ from Elasticsearch import Elasticsearch
 
 # create an Elasticsearch client object
 es = Elasticsearch()
+
+# define the index and document type
+index_name = "my_index"
+doc_type = "my_doc_type"
