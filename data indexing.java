c09a807153301d6bd3066620f4b65d3a7da322f3
attarmau <87364730+attarmau@users.noreply.github.com>
2023-04-18 06:02:26 +0800

    Update data indexing.java

--- data indexing.java
+++ data indexing.java
@@ -6,3 +6,15 @@ es = Elasticsearch()
 # define the index and document type
 index_name = "my_index"
 doc_type = "my_doc_type"
+
+# define the document to be indexed
+doc = {
+    "title": "Example Document",
+    "content": "This is an example document to be indexed in Elasticsearch."
+}
+
+# index the document
+res = es.index(index=index_name, doc_type=doc_type, body=doc)
+
+# print the response
+print(res)
