c8ee7a6b0bf09f5c4293f44a39c76efd12bed3a7
attarmau <87364730+attarmau@users.noreply.github.com>
2023-06-01 08:49:29 +0800

    Update loan predictive.py

--- loan predictive.py
+++ loan predictive.py
@@ -361,7 +361,6 @@ def catboost_classifier(df, test_size=0.2):
     x = df.drop('flag_bad', axis=1) # features
     y = df['flag_bad'] # target variable
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=42)
-
     catboost_clf = CatBoostClassifier(random_seed=42, iterations=500, learning_rate=0.1, loss_function='Logloss')
     catboost_clf.fit(X_train, y_train, verbose=10)
 
