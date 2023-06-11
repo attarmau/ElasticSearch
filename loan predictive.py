f5577428e569fe6b2c22e9fcd531702f3dacf8ef
attarmau <87364730+attarmau@users.noreply.github.com>
2023-06-12 00:03:31 +0800

    Update loan predictive.py

--- loan predictive.py
+++ loan predictive.py
@@ -508,5 +508,4 @@ print("roc_auc:", roc_auc)
 # Further Analysis
 report = sv.analyze(df)
 report.show_html(filepath='Loan_Dataset_Sweetviz.html')
-
 report.show_notebook(filepath='Loan_Dataset_Sweetviz.html')
