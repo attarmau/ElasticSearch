489cfd59ee7af2912febc3c0c1e0a2f025818e81
attarmau <87364730+attarmau@users.noreply.github.com>
2023-06-17 13:39:11 +0800

    Update loan predictive.py

--- loan predictive.py
+++ loan predictive.py
@@ -522,3 +522,12 @@ for i in enumerate(features):
 gender_map = {1: 'male', 2: 'female'}
 df['de_gender'] = df['de_gender'].map(gender_map)
 pd.crosstab(index=df['de_gender'], columns=df['flag_bad'], normalize='index') * 100
+
+# create a crosstab of gender and flag_bad
+gender_bad = pd.crosstab(index=df['de_gender'], columns=df['flag_bad'])
+
+# calculate the proportion of bad loans for each gender
+gender_bad['Proportion of Bad Loans'] = gender_bad[1] / (gender_bad[0] + gender_bad[1])
+
+# result
+print(gender_bad['Proportion of Bad Loans'])
